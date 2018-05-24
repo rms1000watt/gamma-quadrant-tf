4f32d20e5c75f3e64913d68c2935e96c1323fe96
Ryan Smith <rms1000watt@gmail.com>
2018-05-24 08:15:44 -0700

    added compression to cloudfront

--- orgs/gamma-quadrant/global/cloudfront/web.tf
+++ orgs/gamma-quadrant/global/cloudfront/web.tf
@@ -27,6 +27,7 @@ resource "aws_cloudfront_distribution" "web" {
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
+    compress               = true
   }
 
   restrictions {
